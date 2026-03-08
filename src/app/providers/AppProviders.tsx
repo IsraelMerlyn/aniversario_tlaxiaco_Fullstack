23d823da5915fb0f6036370f780cabee52e37cec
Israel Merlyn <merlyn142a@gmail.com>
2026-03-07 21:53:53 -0600

    tasks # corriendo ruta

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,5 +1,6 @@
-import { PropsWithChildren } from "react";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { PropsWithChildren } from "react";
 import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
